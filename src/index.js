5165ad9b260e3ae708a7e302192a9281cbd2da31
Maria Cosmen <mcosme00@gmail.com>
2020-12-11 08:57:29 +0900

    después de starbucks

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@
     let screen = document.getElementById("screen");
     screen.innerText = value;
   }
-
+  // array con valores del 1 al 9 para los números //
   let keys = [];
   for (let i = 0; i < 10; i++) {
     const id = "key" + i;
     const elem = document.getElementById(id);
     keys.push(elem);
 
+    // cuando pulsamos key //
     keys[i].addEventListener("click", () => {
       if (isReset) {
         a = i;
@@ -33,20 +34,6 @@
     });
   }
 
-  function add() {
-    if (op !== undefined) {
-      a = a + b;
-      updateScreen(a);
-      b = 0;
-    }
-    isReset = false;
-  }
-
-  document.getElementById("keyAdd").addEventListener("click", () => {
-    add();
-    op = "add";
-  });
-
   document.getElementById("keyReset").addEventListener("click", reset);
 
   function reset() {
@@ -62,4 +49,27 @@
     op = undefined;
     isReset = true;
   });
+
+  // FUNCIONES DE OPERADORES //
+
+  // 1. Función de suma //
+  function add() {
+    if (op !== undefined) {
+      a = a + b;
+      updateScreen(a);
+      b = 0;
+    }
+    isReset = false;
+  }
+
+  document.getElementById("keyAdd").addEventListener("click", () => {
+    add();
+    op = "add";
+  });
+
+  // 2. Función de resta //
+
+  // 3. Función de multiplicación //
+
+  // 4. Función de división //
 })();
